ba16dfe35d27d4f5920ac0f9c1f6b6187f44dedc
Zach Barham <me@zoweb.me>
2017-08-02 17:26:39 +1000

    Updated class includer

--- browser/include.js
+++ browser/include.js
@@ -5,3 +5,54 @@
  * By using this code, you agree to the license in the file specified (the MIT license)
  */
 
+(function() {
+    let global;
+    if (window) global = window;
+    else if (module && module.exports) global = module.exports;
+
+    let loadingQueue = 0;
+    let included = [];
+
+    let globalLoadStartTime = global.performance && global.performance.now ? global.performance.now() : global.Date.now();
+
+    function checkQueue() {
+        "use strict";
+        if (loadingQueue <= 0) {
+            let globalLoadEndTime = global.performance && global.performance.now ? global.performance.now() : global.Date.now();
+            let globalLoadTime = globalLoadEndTime - globalLoadStartTime;
+
+            console.info("Finished including after " + globalLoadTime.toFixed(3) + "ms");
+
+            global.includes.onload();
+        }
+    }
+
+    global.include = (function() {
+        function include(src) {
+            if (included.indexOf(src) >= 0) return;
+
+            loadingQueue++;
+            included.push(src);
+
+            let script = document.createElement("script");
+            let source = "src/" + src.split(".").join("/") + ".js";
+
+            console.debug(`${new Error().stack.split("@")[2].replace(/\n/, "").split(":")[1].substr(2)} trigged including of ${src} from "${source}"`);
+
+            script.onload = () => {
+                script.remove();
+                loadingQueue--;
+
+                checkQueue();
+            };
+            document.body.appendChild(script);
+            script.src = source;
+        }
+
+        return include;
+    })();
+
+    global.includes = {
+        onload: () => {}
+    }
+}());
