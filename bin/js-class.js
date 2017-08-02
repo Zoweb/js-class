60c1b50ff2b86d7ce1b8af659d4571ddbae1cdcc
Zach Barham <me@zoweb.me>
2017-08-02 19:46:11 +1000

    Moved CLI

--- bin/js-class.js
+++ bin/js-class.js
@@ -1,7 +1,36 @@
-/**
- * Copyright (c) 2017, zoweb
- *
- * See the license in the LICENSE file (downloaded with this repository, in the root folder)
- * By using this code, you agree to the license in the file specified (the MIT license)
- */
+//#!/usr/bin/env node
 
+const cli = require("cli");
+const uglify = require("uglify-es");
+
+const readFile = require("../lib/read-file");
+const generator = require("../lib/generator");
+
+const classTranspiler = require("../");
+
+const options = cli.parse({
+    source: ["s", "Source class file path", "file", false],
+    output: ["o", "Output file path", "file", false]
+});
+
+if (options.source === false) cli.fatal("Invalid or no source file.");
+if (options.output === false) cli.fatal("Invalid or no output file.");
+
+cli.info("Transpiling JS class");
+
+generator(function*(cont) {
+    cli.debug("Reading source file...");
+    let source = yield readFile(options.source, cli.progress).then(cont());
+
+    let result = classTranspiler.transpile(source);
+
+    cli.debug("Uglifying code...");
+
+    let ugly = uglify.minify(result);
+    if (ugly.error) throw ugly.error;
+
+    cli.debug("Saving code...");
+    readFile.saveFile(options.output, ugly.code);
+
+    cli.info("Completed. View the transpiled class at " + options.output);
+})().catch(err => cli.fatal(err.stack));
