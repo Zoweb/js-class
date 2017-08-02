1781f5c2006a9eaed265a6785a1bcc5ae47ca157
Zach Barham <me@zoweb.me>
2017-08-02 19:45:36 +1000

    Add example files

---
+++ data/example.js
@@ -0,0 +1,14 @@
+// Define the namespace
+namespace My.Namespace {
+    // Import other classes
+import Another.Namespace.Class;
+import Or.Library;
+
+    // Define class
+    class MyClass {
+        // ... like JS classes
+        constructor() {
+            return "foo";
+        }
+    }
+}
