2c2826be8b337a2db73a66f33e8bf40daf78f43a
Zach Barham <me@zoweb.me>
2017-08-02 19:59:15 +1000

    Fix more Windows Script Host problems

--- bin/js-class.js
+++ bin/js-class.js
@@ -1,36 +1,36 @@
-//#!/usr/bin/env node
+module.exports = () => {
+    const cli = require("cli");
+    const uglify = require("uglify-es");
 
-var cli = require("cli");
-var uglify = require("uglify-es");
+    const readFile = require("../lib/read-file");
+    const generator = require("../lib/generator");
 
-var readFile = require("../lib/read-file");
-var generator = require("../lib/generator");
+    const classTranspiler = require("../");
 
-var classTranspiler = require("../");
+    const options = cli.parse({
+        source: ["s", "Source class file path", "file", false],
+        output: ["o", "Output file path", "file", false]
+    });
 
-var options = cli.parse({
-    source: ["s", "Source class file path", "file", false],
-    output: ["o", "Output file path", "file", false]
-});
+    if (options.source === false) cli.fatal("Invalid or no source file.");
+    if (options.output === false) cli.fatal("Invalid or no output file.");
 
-if (options.source === false) cli.fatal("Invalid or no source file.");
-if (options.output === false) cli.fatal("Invalid or no output file.");
+    cli.info("Transpiling JS class");
 
-cli.info("Transpiling JS class");
+    generator(function*(cont) {
+        cli.debug("Reading source file...");
+        const source = yield readFile(options.source, cli.progress).then(cont());
 
-generator(function*(cont) {
-    cli.debug("Reading source file...");
-    var source = yield readFile(options.source, cli.progress).then(cont());
+        const result = classTranspiler.transpile(source);
 
-    var result = classTranspiler.transpile(source);
+        cli.debug("Uglifying code...");
 
-    cli.debug("Uglifying code...");
+        const ugly = uglify.minify(result);
+        if (ugly.error) throw ugly.error;
 
-    var ugly = uglify.minify(result);
-    if (ugly.error) throw ugly.error;
+        cli.debug("Saving code...");
+        readFile.saveFile(options.output, ugly.code);
 
-    cli.debug("Saving code...");
-    readFile.saveFile(options.output, ugly.code);
-
-    cli.info("Compvared. View the transpiled class at " + options.output);
-})().catch(function(err) {cli.fatal(err.stack)});
+        cli.info("Compvared. View the transpiled class at " + options.output);
+    })().catch(function(err) {cli.fatal(err.stack)});
+};
